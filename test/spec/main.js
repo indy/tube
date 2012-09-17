6b41778449974b4abe789fc37d8939d8d769f370
Inderjit Gill <email@indy.io>
2012-09-17 20:47:45 +0100

    testing pqueue works

---
+++ test/spec/main.js
@@ -0,0 +1,56 @@
+require.config({
+  shim: {
+  },
+
+  //if the module ID starts with "scripts",
+  //load it from the ../../app/scripts directory. 
+  paths: {
+    scripts: '../../app/scripts'
+  }
+});
+
+require(['pqueue.test'], function() {
+/*
+  var runner = mocha.run();
+
+  if(!window.PHANTOMJS) return;
+
+  runner.on('test', function(test) {
+    sendMessage('testStart', test.title);
+  });
+
+  runner.on('test end', function(test) {
+    sendMessage('testDone', test.title, test.state);
+  });
+
+  runner.on('suite', function(suite) {
+    sendMessage('suiteStart', suite.title);
+  });
+
+  runner.on('suite end', function(suite) {
+    if (suite.root) return;
+    sendMessage('suiteDone', suite.title);
+  });
+
+  runner.on('fail', function(test, err) {
+    sendMessage('testFail', test.title, err);
+  });
+
+  runner.on('end', function() {
+    var output = {
+      failed  : this.failures,
+      passed  : this.total - this.failures,
+      total   : this.total
+    };
+
+    sendMessage('done', output.failed,output.passed, output.total);
+  });
+
+  function sendMessage() {
+    var args = [].slice.call(arguments);
+    alert(JSON.stringify(args));
+  }
+*/
+
+});
+
