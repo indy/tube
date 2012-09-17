7a59483016529dc15facd72211630e34254ca07c
Inderjit Gill <email@indy.io>
2012-09-17 18:55:17 +0100

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,14 @@
+require.config({
+  shim: {
+  },
+
+  paths: {
+    hm: 'vendor/hm',
+    esprima: 'vendor/esprima',
+    jquery: 'vendor/jquery.min'
+  }
+});
+ 
+require(['app'], function(app) {
+  console.log(app);
+});
